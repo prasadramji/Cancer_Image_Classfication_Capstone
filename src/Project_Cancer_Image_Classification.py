026090d55e3af4c11954b52cfb1302ac6dc38aee
Ramji <c-prasad.ramji@thermofisher.com>
2021-11-28 20:53:01 -0800

    code updated

---
+++ src/Project_Cancer_Image_Classification.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import os
+import numpy as np
+import time
+from PIL import Image
+import resnet_model as cm
+
+
+st.title("Cancer Image Classification")
+
+st.markdown("\nsome text")
+
+col1 = st.beta_columns(2)
+image_1 = col1.file_uploader("Image ",type=['png','jpg','jpeg'])
+
+col1,col2 = st.beta_columns(2)
+predict_button = col1.button('Predict on uploaded files')
+test_data = col2.button('Predict on sample data')
+
+@st.cache
+def create_model():
+    resmodel = cm.create_model()
+    return resmodel
+
+
+def predict(image_1,resmodel,predict_button = predict_button):
+    start = time.process_time()
+    if predict_button:
+        if (image_1 is not None):
+            start = time.process_time()  
+            image_1 = Image.open(image_1).convert("RGB") #converting to 3 channels
+            image_1 = np.array(image_1)/255
+            st.image([image_1],width=300)
+            caption = cm.predict_Category([image_1],resmodel)
+            st.markdown(" ### **Impression:**")
+            impression = st.empty()
+            impression.write(caption[0])
+            time_taken = "Time Taken for prediction: %i seconds"%(time.process_time()-start)
+            st.write(time_taken)
+            del image_1
+        else:
+            st.markdown("## Upload an Image")
+
+def predict_sample(resmodel,folder = './test_images'):
+    no_files = len(os.listdir(folder))
+    file = np.random.randint(1,no_files)
+    file_path = os.path.join(folder,str(file))
+    image_1 = os.path.join(file_path,os.listdir(file_path)[0])
+    predict(image_1,resmodel, True)
+    
+
+resmodel = create_model()
+
+
+if test_data:
+    predict_sample(resmodel)
+else:
+    predict(image_1,resmodel)
--- src/resnet_model.py
+++ src/resnet_model.py
@@ -1,252 +1,50 @@
-import time
-import json
-from torch.utils.data import DataLoader
-from PIL import Image
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import (models,
-                         transforms)
-import copy
+import joblib
+import os
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.applications import ResNet101
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.preprocessing import image
+from keras.applications.resnet101 import preprocess_input
 
 
-class ResnetModel:
+def create_model():
+    """
+    creates the best model ie the attention model
+    and returns the model after loading the weights
+    """
+    Image_Size=[224,224]
 
-    def __init__(self, path_to_pretrained_model=None, map_location='cpu', num_classes=5):
-        """
-        Allows for training, evaluation, and prediction of ResNet Models
+    resnet=Sequential()
 
-        params
-        ---------------
-        path_to_pretrained_model - string - relative path to pretrained model - default None
-        map_location - string - device to put model on - default cpu
-        num_classes - int - number of classes to put on the deheaded ResNet
-        """
-        self.device = torch.device(
-            'cuda:0' if torch.cuda.is_available() else 'cpu')
-       
-        self.model = torch.load(
-            path_to_pretrained_model, map_location=self.device
-        )
+    resnet.add(ResNet101(input_shape=Image_Size+[3],weights='imagenet',include_top=False,pooling='average'))
 
-    def feed_forward(self, model, inputs):
-        """
-        Feeds inputs through model
+    # This is to ensure the base won't be trained again
+    for layer in resnet.layers:
+        layer.trainable=False
+        
+    flatten=Flatten()(resnet.output)
+    dense=Dense(256,activation='relu')(flatten)
+    dense=Dense(128,activation='relu')(dense)
+    dense=Dropout(0.2)(dense)   # We add a dropout here to prevent overfitting
+    prediction=Dense(5,activation='softmax')(dense)
 
-        params
-        ---------------
-        model - torch model to feed inputs through
-        inputs - inputs to feed through model
+    resmodel=Model(inputs=resnet.input,outputs=prediction)
+    resmodel.load_weights('../models/resnet101_weights.h5')
+    return resmodel
 
-        returns
-        ---------------
-        output_tensor - torch tensor - tensor of final output layer after feed forward
-        """
-        output_tensor = model(inputs)
-        return output_tensor
+def predict_Category(image1, model):
+    """given image1 filepaths returns the predicted caption
+    """
 
-    def fit(self, train_loader, val_loader, num_epochs=10, criterion=None,
-            optimizer=None, batch_size=16, early_stop_min_increase=0.003,
-            early_stop_patience=10, lr=0.0001):
-        """
-        Impliments transfer learning based on new data
+    # img = image.load_img(img_path, target_size=(224,224))
+    # x = image.img_to_array(image1)
+    x = np.expand_dims(image1, axis=0)
+    x = preprocess_input(x)
 
-        params
-        ---------------
-        train_loader - torch DataLoader - Configured DataLoader for training, helpful when images are flowing from folder
-        val_loader - torch DataLoader - Configured DataLoader for validation, helpful when images are flowing from folder
-        num_epochs - int - number of epochs to use during training
-        criterion - Loss function to assess model during training and evaluation - default None and CrossEntropyLoss
-        optimizer - Optimizer algorithm - default Adam
-        batch_size - int - Number of images to use per optimizer update - default 16
-        early_stop_min_increase - float - Minimum increase in accuracy score to indicate model improvement per epoch - default 0.003
-        early_stop_patience - int - Number of epochs to allow less than or equal to minimum improvement - default 10
-        lr - float - Learning rate for optimization algorithm - default 0.0001
-
-        returns
-        ---------------
-        model - trained torch model
-        loss - list - history of loss across epochs
-        acc - list - history of accuracy across epochs
-        """
-        # TODO: change data loader params to data params then in this function,
-        # Transform those to data loaders so that the batch size can be dynamic
-        start = time.time()
-        model = self.model
-        best_model_wts = copy.deepcopy(self.model.state_dict())
-        best_acc = 0
-        train_loss_over_time = []
-        val_loss_over_time = []
-        train_acc_over_time = []
-        val_acc_over_time = []
-        epochs_no_improve = 0
-        early_stop = False
-        phases = ['train', 'val']
-
-        if not optimizer:
-            optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        if not criterion:
-            criterion = nn.CrossEntropyLoss()
-
-        for epoch in range(num_epochs):
-            print(f"Epoch number: {epoch + 1} / {num_epochs}")
-
-            for phase in phases:
-                if phase == 'train':
-                    data_loader = train_loader
-                    model.train()
-                else:
-                    data_loader = val_loader
-                    model.eval()
-
-                running_loss = 0
-                running_corrects = 0
-
-                for inputs, labels in data_loader:
-                    inputs = inputs.to(self.device)
-                    labels = labels.to(self.device)
-
-                    optimizer.zero_grad()
-
-                    with torch.set_grad_enabled(phase == 'train'):
-                        outputs = self.feed_forward(model, inputs)
-                        _, pred = torch.max(outputs, dim=1)
-                        loss = criterion(outputs, labels)
-
-                        if phase == 'train':
-                            loss.backward()
-                            optimizer.step()
-
-                    running_loss += loss.item() * inputs.size(0)
-                    running_corrects += torch.sum(pred == labels.data)
-
-                if phase == 'train':
-                    epoch_loss = running_loss / len(train_loader.dataset)
-                    train_loss_over_time.append(epoch_loss)
-                    epoch_acc = running_corrects.double() / len(train_loader.dataset)
-                    train_acc_over_time.append(epoch_acc)
-
-                else:
-                    epoch_loss = running_loss / len(val_loader.dataset)
-                    val_loss_over_time.append(epoch_loss)
-                    epoch_acc = running_corrects.double() / len(val_loader.dataset)
-                    val_acc_over_time.append(epoch_acc)
-
-                print(f"{phase} loss: {epoch_loss:.3f}, acc: {epoch_acc:.3f}")
-
-                if phase == 'val' and epoch_acc > best_acc:
-                    best_acc = epoch_acc
-                    best_model_wts = copy.deepcopy(model.state_dict())
-                    torch.save(model, 'trained_model_resnet50_checkpoint.pt')
-                    epochs_no_improve = 0
-
-                elif phase == 'val' and (epoch_acc - best_acc) < early_stop_min_increase:
-                    epochs_no_improve += 1
-                    print(
-                        f"Number of epochs without improvement has increased to {epochs_no_improve}")
-
-            if epochs_no_improve >= early_stop_patience:
-                early_stop = True
-                print('Early stopping!')
-                break
-
-            print('-' * 60)
-            total_time = (time.time() - start) / 60
-            print(
-                f"Training completed. Time taken: {total_time:.3f} min\nBest accuracy: {best_acc:.3f}")
-            model.load_state_dict(best_model_wts)
-            self.model = model
-            loss = {'train': train_loss_over_time, 'val': val_loss_over_time}
-            acc = {'train': train_acc_over_time, 'val': val_acc_over_time}
-
-            return model, loss, acc
-
-    def evaluate(self, test_loader, model=None, criterion=None):
-        """
-        Feeds set of images through model and evaluates relevant metrics
-        as well as batch predicts. Prints loss and accuracy
-
-        params
-        ---------------
-        test_loader - torch DataLoader - Configured DataLoader for evaluation, helpful when images flow from directory
-        model - trained torch model - Model to use during evaluation - default None which retrieves model from attributes
-        criterion - Loss function to assess model - Default None which equates to CrossEntropyLoss.
-
-        returns
-        ---------------
-        preds - list - List of predictions to use for evaluation of non-included metrics
-        labels_list - list - List of labels to use for evaluation of non-included metrics
-        """
-        if not model:
-            model = self.model
-        if not criterion:
-            criterion = nn.CrossEntropyLoss()
-
-        # model = self.model
-        model.eval()
-        test_loss = 0
-        test_acc = 0
-        preds = list()
-        labels_list = list()
-
-        for inputs, labels in test_loader:
-            inputs = inputs.to(self.device)
-            labels = labels.to(self.device)
-
-            with torch.no_grad():
-                outputs = self.feed_forward(model, inputs)
-                loss = criterion(outputs, labels)
-                _, pred = torch.max(outputs, dim=1)
-                preds.append(pred.item())
-                labels_list.append(labels.item())
-
-            test_loss += loss.item() * inputs.size(0)
-            correct = pred.eq(labels.data.view_as(pred))
-            accuracy = torch.mean(correct.type(torch.FloatTensor))
-            test_acc += accuracy.item() * inputs.size(0)
-
-        test_loss = test_loss / len(test_loader.dataset)
-        test_acc = test_acc / len(test_loader.dataset)
-
-        print(f"Test loss: {test_loss:.4f}\nTest acc: {test_acc:.4f}")
-
-        return preds, labels_list
-
-    def predict_proba(self, img, k, index_to_class_labels, show=False):
-        """
-        Feeds single image through network and returns top k predicted labels and probabilities
-
-        params
-        ---------------
-        img - PIL Image - Single image to feed through model
-        k - int - Number of top predictions to return
-        index_to_class_labels - dict - Dictionary to map indices to class labels
-        show - bool - Whether or not to display the image before prediction - default False
-
-        returns
-        ---------------
-        formatted_predictions - list - List of top k formatted predictions formatted to include a tuple of 1. predicted label, 2. predicted probability as str
-        """
-        if show:
-            img.show()
-        # img = self.test_transform(img)
-        # img = img.unsqueeze(0)
-        # img = img.to(self.device)
-        self.model.eval()
-        output_tensor = self.feed_forward(self.model, img)
-        prob_tensor = torch.nn.Softmax(dim=1)(output_tensor)
-        top_k = torch.topk(prob_tensor, k, dim=1)
-        probabilites = top_k.values.detach().numpy().flatten()
-        indices = top_k.indices.detach().numpy().flatten()
-        formatted_predictions = []
-
-        for pred_prob, pred_idx in zip(probabilites, indices):
-            predicted_label = index_to_class_labels[pred_idx].title()
-            predicted_perc = pred_prob * 100
-            formatted_predictions.append(
-                (predicted_label, f"{predicted_perc:.3f}%"))
-
-        return formatted_predictions
-
-    # change predict proba to actually be in line with sklearn's API and create another function that formats the raw probabilities.
-    # Then go back in to the website, and change the code accordingly.
+    return model.predict(x)
